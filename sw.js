6343fdfe165552edf9f3f682c25b9e2300f03041
suhail24uddin-droid <suhail24uddin@gmail.com>
2026-07-08 16:31:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meetnotes-v16';
+const CACHE_NAME = 'meetnotes-v17';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
